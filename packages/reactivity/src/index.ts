00360c40606872a48b4296bb61609e644ae2515e
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-07-17 14:47:28 +0400

    :white_check_mark: Expands testing

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,47 +1,51 @@
 import { nextTick } from './nextTick';
 
 let nextId = 1;
-
+const DEBUG = false;
 export class Signal<T = unknown> {
   id: number;
   value: T;
   dependents: Set<Effect> = new Set();
   constructor(value: T) {
     this.id = nextId++;
-    console.log(
-      'creating Signal:',
-      typeof value === 'object' ? 'object' : value,
-      this.id,
-    );
+    DEBUG &&
+      console.log(
+        'creating Signal:',
+        typeof value === 'object' ? 'object' : value,
+        this.id,
+      );
     this.value = value;
   }
   get() {
-    console.log(
-      'getting Signal:',
-      typeof this.value === 'object' ? 'object' : this.value,
-      this.id,
-    );
+    DEBUG &&
+      console.log(
+        'getting Signal:',
+        typeof this.value === 'object' ? 'object' : this.value,
+        this.id,
+      );
     if (dontTrack) return this.value;
     const activeEffect = effectStack.at(-1);
     if (activeEffect) {
-      console.log(
-        'registering signal:',
-        this.id,
-        ', to effect:',
-        activeEffect.id,
-      );
+      DEBUG &&
+        console.log(
+          'registering signal:',
+          this.id,
+          ', to effect:',
+          activeEffect.id,
+        );
       activeEffect.register(this);
       this.dependents.add(activeEffect);
     }
     return this.value;
   }
   set(value: T) {
-    console.log('setting signal:', value, this.id);
+    DEBUG && console.log('setting signal:', value, this.id);
     this.value = value;
     this.dependents.forEach((effect) => effect.rerun());
   }
   release(effect: Effect) {
-    console.log('releasing effect:', effect.id, ', from signal:', this.id);
+    DEBUG &&
+      console.log('releasing effect:', effect.id, ', from signal:', this.id);
     this.dependents.delete(effect);
   }
 }
@@ -73,27 +77,33 @@ export class Effect {
   dependencies: Set<Signal> = new Set();
   constructor(cb: () => void) {
     this.id = nextId++;
-    console.log('creating effect:', this.id);
+    DEBUG && console.log('creating effect:', this.id);
     this.operation = cb;
     this.run();
   }
   run() {
     effectStack.push(this);
-    console.log('running effect:', this.id);
+    DEBUG && console.log('running effect:', this.id);
     this.operation();
     effectStack.pop();
   }
   register(signal: Signal) {
-    console.log('registering signal:', signal.id, ', to this effect:', this.id);
+    DEBUG &&
+      console.log(
+        'registering signal:',
+        signal.id,
+        ', to this effect:',
+        this.id,
+      );
     this.dependencies.add(signal);
   }
   rerun() {
-    console.log('triggering rerun on effect:', this.id);
+    DEBUG && console.log('triggering rerun on effect:', this.id);
     this.release();
     addToEffectQueue(this);
   }
   release() {
-    console.log('releasing effect', this.id);
+    DEBUG && console.log('releasing effect', this.id);
     this.dependencies.forEach((signal) => {
       signal.release(this);
       this.dependencies.delete(signal);
@@ -155,7 +165,83 @@ const wrap = <T>(item: T): Signal<T> => {
 
 if (import.meta.vitest) {
   describe('reactivity', () => {
-    it('has reactivity', async () => {
+    it('creates signals wrapping values', () => {
+      const signal = new Signal(1);
+      expect(signal.get()).toBe(1);
+      signal.set(42);
+      expect(signal.get()).toBe(42);
+    });
+    it('can register reactive effects', async () => {
+      const signal = new Signal(1);
+      let value: number = 0;
+      new Effect(() => (value = signal.get()));
+      expect(value).toBe(1);
+      signal.set(42);
+      expect(value).toBe(1);
+      await nextTick();
+      expect(value).toBe(42);
+    });
+    it('disconnects all signals tracking when effect is triggered', async () => {
+      const first = new Signal(5);
+      const second = new Signal(10);
+      let value: number = 0;
+      const effect = new Effect(() => (value = first.get() + second.get()));
+      expect(value).toBe(15);
+      expect(first.dependents.size).toBe(1);
+      expect(second.dependents.size).toBe(1);
+      expect(effect.dependencies.size).toBe(2);
+      first.set(42);
+      expect(first.dependents.size).toBe(0);
+      expect(second.dependents.size).toBe(0);
+      expect(effect.dependencies.size).toBe(0);
+      await nextTick();
+      expect(value).toBe(52);
+      expect(first.dependents.size).toBe(1);
+      expect(second.dependents.size).toBe(1);
+      expect(effect.dependencies.size).toBe(2);
+    });
+    it('can release effects', async () => {
+      const signal = new Signal(1);
+      let value: number = 0;
+      const effect = new Effect(() => (value = signal.get()));
+      expect(value).toBe(1);
+      signal.set(42);
+      await nextTick();
+      expect(value).toBe(42);
+      effect.release();
+      signal.set(100);
+      await nextTick();
+      expect(value).toBe(42);
+    });
+    it('can run released effects', async () => {
+      const signal = new Signal(1);
+      let value: number = 0;
+      const effect = new Effect(() => (value = signal.get()));
+      expect(value).toBe(1);
+      signal.set(42);
+      await nextTick();
+      expect(value).toBe(42);
+      effect.release();
+      signal.set(100);
+      await nextTick();
+      expect(value).toBe(42);
+      effect.run();
+      expect(value).toBe(100);
+    });
+    it('can create reactive objects', async () => {
+      const data = reactive({
+        foo: 0b1,
+        bar: 0b10,
+      });
+      let value: number = 0b0;
+      new Effect(() => (value = data.foo + data.bar));
+      expect(value).toBe(0b11);
+      data.foo = 0b100;
+      expect(value).toBe(0b11);
+      await nextTick();
+      expect(value).toBe(0b110);
+    });
+    it('handled nested reactive objects', async () => {
       const info = reactive({
         names: {
           first: 'Foo',
@@ -163,9 +249,7 @@ if (import.meta.vitest) {
         },
       });
       let value: string = '';
-      const effect = new Effect(
-        () => (value = `${info.names.first} ${info.names.last}`),
-      );
+      new Effect(() => (value = `${info.names.first} ${info.names.last}`));
       expect(value).toBe('Foo Bar');
       info.names.first = 'Fizz';
       expect(value).toBe('Foo Bar');
@@ -174,27 +258,32 @@ if (import.meta.vitest) {
       expect(value).toBe('Foo Bar');
       await nextTick();
       expect(value).toBe('Fizz Buzz');
-      effect.release();
-
-      info.names.first = 'Tony';
-      info.names.last = 'Stark';
-      expect(value).toBe('Fizz Buzz');
-
-      effect.run();
-      expect(value).toBe('Tony Stark');
-
       info.names = {
         first: 'Peter',
         last: 'Parker',
       };
-
-      expect(value).toBe('Tony Stark');
-
+      expect(value).toBe('Fizz Buzz');
       await nextTick();
       expect(value).toBe('Peter Parker');
       info.names.last = 'Porker';
       await nextTick();
       expect(value).toBe('Peter Porker');
+    });
+    it('can untrack inside an effect', async () => {
+      const first = new Signal(5);
+      const second = new Signal(10);
+      let value: number = 0;
+      new Effect(() => {
+        const initial = first.get();
+        untrack(() => (value = initial + second.get()));
+      });
+      expect(value).toBe(15);
+      first.set(42);
+      await nextTick();
+      expect(value).toBe(52);
+      second.set(100);
+      await nextTick();
+      expect(value).toBe(52);
     });
   });
 }

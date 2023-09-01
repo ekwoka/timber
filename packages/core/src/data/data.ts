ab6e70cbfb9dda882a123c56c8f0845a776150bb
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-09-01 21:23:17 +0400

    :white_check_mark: Tests Scope

--- packages/core/src/data/data.ts
+++ packages/core/src/data/data.ts
@@ -1,3 +1,5 @@
+// @vitest-environment happy-dom
+
 const elementContext = new WeakMap<Element, ComponentContext>();
 
 export const addScopeToNode = (el: Element, scope: ArbitraryData) => {
@@ -21,3 +23,22 @@ export interface ComponentContext {
 }
 
 export type ArbitraryData = Record<string | symbol | number, unknown>;
+
+if (import.meta.vitest) {
+  describe('scope', () => {
+    it('adds scope to the node', () => {
+      const node = document.createElement('div');
+      const scope = { foo: 'bar' };
+      addScopeToNode(node, scope);
+      expect(getContext(node)).toEqual({ data: scope });
+    });
+    it('can find the nearest context', () => {
+      const node = document.createElement('div');
+      const child = document.createElement('div');
+      const scope = { foo: 'bar' };
+      addScopeToNode(node, scope);
+      node.append(child);
+      expect(nearestContext(child)).toEqual({ data: scope });
+    });
+  });
+}
